c0ee53e79902e89a166df5b76fce10f3edb682d0
Daltro-Oliveira-Vinuto <daltroov777@gmail.com>
2026-01-20 00:32:15 -0300

    makefile created with success

---
+++ makefile
@@ -0,0 +1,45 @@
+all: compile generate_cap
+
+compile:
+	javac -version
+	@echo 
+
+	javac -source 1.2 -target 1.2 \
+	-cp ~/javacard-sdk-3.0.5/lib/api_classic.jar \
+	-d bin \
+	src/main/java/card/HelloWorld.java
+
+	@echo "Compilation finished, file *.class created\n"
+
+JC_SDK :=/home/daltro/java_card_kit-2_2_2-rr-bin-linux-do
+generate_cap: 
+	java -cp $(JC_SDK)/lib/converter.jar:$(JC_SDK)/lib/api.jar \
+	com.sun.javacard.converter.Converter \
+	-classdir bin \
+	-exportpath $(JC_SDK)/api_export_files \
+	-applet 0xA0:0x00:0x00:0x00:0x62:0x03:0x01:0x0C:0x01 card.HelloWorld \
+	card 0xA0:0x00:0x00:0x00:0x62:0x03:0x01:0x0C 1.0 \
+	-noverify
+
+	@echo "*.cap file created with success\n"
+	 
+
+java11 :=/opt/java/jdk-11/bin/java
+gp := ~/gp_v25.10.20/gp.jar
+run_gp :=$(java11) -jar  $(gp)
+install_cap:
+	$(run_gp) -install bin/card/javacard/card.cap
+
+
+cap_instance := A00000006203010C01
+cap_package := A00000006203010C
+delete_cap:
+	rm -rf bin/card/*
+	$(run_gp) --delete $(cap_instance)
+	$(run_gp) --delete $(cap_package)
+
+
+apdu_1 := 00A4040009A00000006203010C01
+apdu_2 := 80010000
+send_apdu:
+	$(run_gp) -a $(apdu_1) -a $(apdu_2)
--- src/main/java/card/HelloWorld.java
+++ src/main/java/card/HelloWorld.java
@@ -3,16 +3,32 @@ package card;
 import javacard.framework.*;
 
 public class HelloWorld extends Applet {
+    // Definimos uma instrução para pedir o texto (0x01)
+    private static final byte INS_SAY_HELLO = (byte) 0x01;
+    private static final byte[] helloText = {
+        'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'
+    };
+
     public static void install(byte[] bArray, short bOffset, byte bLength) {
         new HelloWorld().register();
     }
-    
 
     public void process(APDU apdu) {
         if (selectingApplet()) {
             return;
         }
-        // Retorna apenas sucesso (9000)
+
+        byte[] buffer = apdu.getBuffer();
+        
+        // Verifica se a instrução enviada é a 0x01
+        if (buffer[ISO7816.OFFSET_INS] == INS_SAY_HELLO) {
+            short len = (short) helloText.length;
+            // Copia o "Hello World" para o buffer de saída
+            Util.arrayCopyNonAtomic(helloText, (short) 0, buffer, (short) 0, len);
+            // Envia os bytes de volta
+            apdu.setOutgoingAndSend((short) 0, len);
+        } else {
+            ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+        }
     }
 }
-
